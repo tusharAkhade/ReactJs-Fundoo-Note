0f1046c65a7f09967b3c974bb6a62bed0530e558
Tushar Akhade <tusharakhade10@gmail.com>
2021-10-16 05:38:29 +0530

    Added: restore button

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -9,6 +9,8 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import TimePicker from "@mui/lab/TimePicker";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+import RestoreFromTrashIcon from '@mui/icons-material/RestoreFromTrash';
+import { connect } from 'react-redux';
 
 function Icons(props) {
 
@@ -16,7 +18,6 @@ function Icons(props) {
     const [openCollab, setopenCollab] = useState(true)
     const [anchorEl, setAnchorEl] = useState(null);
     const [value, setValue] = useState(new Date());
-
 
     const handleChange = (newValue) => {
         setValue(newValue);
@@ -36,6 +37,10 @@ function Icons(props) {
         if (props.name == "ViewNote") {
             props.deletedNote()
         }
+    }
+
+    let handleRestoreClick = () => {
+        props.restoreNote()
     }
 
     const listenToCollab = () => {
@@ -66,7 +71,7 @@ function Icons(props) {
 
             <div className="noteTwoIcon">
                 <div className={props.type == "displayNotes" ? "iconOfDisplayNotes" : "multipleIconsOfNote2"}>
-                    <div className="iconOfNote2" id="iconOfNote2-1" onClick={handleReminderClick} ></div>
+                    <div className="iconOfNote2" id={props.noteType == "Bin" ? "hideIcon" : "iconOfNote2-1"} onClick={handleReminderClick} ></div>
                     <Popper click style={{ boxShadow: "1px 1px 5px grey", backgroundColor: "#fff", border: "2px solid white", width: "300px", borderRadius: "3px", height: "200px" }} id={id} open={open} anchorEl={anchorEl}>
                         <LocalizationProvider dateAdapter={AdapterDateFns}>
                             <DesktopDatePicker
@@ -85,15 +90,16 @@ function Icons(props) {
                         </LocalizationProvider>
                         <button onClick={handleReminderSaveClick} style={{ width: "70px", height: "30px", margin: "10px" }}>Save</button>
                     </Popper>
-                    <div className="iconOfNote2" id="iconOfNote2-2" onClick={listenToCollab} ></div>
-                    <div className="iconOfNote2" id="iconOfNote2-3"> <ColorPopper className="iconOfNote2" getColor={props.getColor} /> </div>
-                    <div className="iconOfNote2" id="iconOfNote2-4"></div>
-                    <div className="iconOfNote2" id="iconOfNote2-5" onClick={handleArchiveClick} ></div>
-                    <div className="iconOfNote2" id={props.type == "removeDeleteIconFromNoteTwoIcons" ? "removeDeleteIcon" : "iconOfNote2-6"} onClick={handleDeleteClick} >
+                    <div className="iconOfNote2" id={props.noteType == "Bin" ? "hideIcon" : "iconOfNote2-2"} onClick={listenToCollab} ></div>
+                    <div className="iconOfNote2" id={props.noteType == "Bin" ? "hideIcon" : "iconOfNote2-3"}> <ColorPopper className="iconOfNote2" getColor={props.getColor} /> </div>
+                    <div className="iconOfNote2" id={props.noteType == "Bin" ? "hideIcon" : "iconOfNote2-4"}></div>
+                    <div className="iconOfNote2" id={props.noteType == "Bin" ? "hideIcon" : "iconOfNote2-5"} onClick={handleArchiveClick} ></div>
+                    <div className="iconOfNote2" id={props.type == "removeDeleteIconFromNoteTwoIcons" ? "hideIcon" : "iconOfNote2-6"} onClick={handleDeleteClick} >
                         <DeleteOutlineOutlinedIcon />
                     </div>
                     <div className="iconOfNote2" id={props.type == "displayNotes" ? "iconOfDisplayNotes2-7" : "iconOfNote2-7"}></div>
                     <div className="iconOfNote2" id={props.type == "displayNotes" ? "iconOfDisplayNotes2-8" : "iconOfNote2-8"}></div>
+                    <div className="iconOfNote2" id={props.noteType == "Bin" ? "viewRestoreIcon" : "hideIcon"} onClick={handleRestoreClick}> <RestoreFromTrashIcon /> </div>
                 </div>
                 <div className="closeBtnContainer">
                     <div className={props.type == "displayNotes" ? "closeButtonIconOfDisplayNotes2" : "closeButtonIconOfNote2"} role="button" onClick={props.close}> Close </div>
@@ -103,4 +109,10 @@ function Icons(props) {
     )
 }
 
-export default Icons
+const mapStateToProps = (state) => {
+    return {
+        noteType: state.clicked
+    }
+}
+
+export default connect(mapStateToProps)(Icons)
